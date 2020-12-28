d78a675dc65907842a0bf600cdbabb569ea06cd4
Andriy V. Koval <koval.andrey@gmail.com>
2020-12-28 13:11:33 -0500

    Update 1-first-look.R

--- analysis/1-first-look/1-first-look.R
+++ analysis/1-first-look/1-first-look.R
@@ -30,12 +30,14 @@ ds0 <- readr::read_rds(path_input)
 # metadata <- readr::read_csv("../../data-public/metadata/better-labels.csv")
 metadata <- readr::read_csv("./data-public/metadata/better-labels.csv")
 
-ds0 %>% glimpse()
+# ds0 %>% glimpse()
 # ---- inspect-data -----------------------------------------------------------
 skimr::skim(ds0)
 ds0 %>% pryr::object_size()
-# -----tweak-data -------------------------------------------------------------
 
+# -----tweak-data-1 -------------------------------------------------------------
+
+# ds1 - adjust the rows
 ds1 <- ds0 %>%
   mutate(
     date_received = as.Date(paste0(year_received,"-",month_received,"-15"))
@@ -55,14 +57,19 @@ ds1 <- ds0 %>%
                                         ,"Other" = "Transwoman (MTF)"
                                         ,"(Missing)" = "Not described"
                                         )
+    ,complainant_age_incident = ifelse(complainant_age_incident < 10,NA,complainant_age_incident)
+
   ) %>%
   mutate_at(
     c("complainant_ethnicity","mos_ethnicity"), factor
   )
   # select(-c("year_received","month_received","year_closed","month_closed"))
 
-ds1 %>% glimpse()
+# ds1 %>% glimpse()
 
+# -----tweak-data-2 -------------------------------------------------------------
+
+# ds2 - adjust the columns
 ds2 <- ds1 %>%
   select(
   # MOS - Member of Service
@@ -72,7 +79,6 @@ ds2 <- ds1 %>%
     ,mos_age_incident        #
     ,mos_yob                 # Year of birth
     ,rank_now                # Officer's rank as of July 2020
-    --
     ,rank_incident           #  --- at the time of the incident
     ,rank_abbrev_now         #
     ,rank_abbrev_incident    #
@@ -105,69 +111,146 @@ ds2 <- ds1 %>%
 # ds2 %>% glimpse()
 # setdiff(names(ds1),names(ds2))
 #
+
+# ----- variable-groups ---------------------
+metadata %>%
+  group_by(entity) %>%
+  summarize(n_variables = n_distinct(item_name)) %>%
+  arrange(desc(entity)) %>%
+  neat()
+
+metadata %>%
+  filter(entity == "officer") %>%
+  arrange(desc(entity), item_name) %>%
+  select(entity,item_name, item_description, item_label) %>%
+  neat()
+
+metadata %>%
+  filter(entity == "complainant") %>%
+  arrange(desc(entity), item_name) %>%
+  select(entity,item_name, item_description, item_label) %>%
+  neat()
+
+metadata %>%
+  filter(entity == "complaint") %>%
+  arrange(desc(entity), item_name) %>%
+  select(entity,item_name, item_description, item_label) %>%
+  neat()
+
+metadata %>%
+  filter(entity == "contact") %>%
+  arrange(desc(entity), item_name) %>%
+  select(entity,item_name, item_description, item_label) %>%
+  neat()
 # ---- officer-1 -------------------------
 
+# ---- officer-2 -------------------------
 # How many distinct officers and complaints represented in the data?
 ds2 %>% summarize(
   n_officers = n_distinct(unique_mos_id)
   ,n_shield_ns = n_distinct(shield_no)
   ,n_complaints = n_distinct(complaint_id)
-  )
+  ) %>%
+  neat()
 
 # how many missing shield_no?
 ds2 %>% filter(shield_no == 0L) %>%
-  summarize(n_mos_with_missing_shield_no = n_distinct(unique_mos_id))
+  summarize(n_mos_with_missing_shield_no = n_distinct(unique_mos_id))%>%
+  neat()
+
 # Shield No is missing from 458 officers
 
 # Are shield numbers unique?
 ds2 %>% group_by(shield_no) %>%
   summarize(n_duplicate_shields = n_distinct(unique_mos_id)) %>%
   filter(n_duplicate_shields > 1) %>%
-  arrange(desc(n_duplicate_shields))
+  arrange(desc(n_duplicate_shields)) %>%
+  slice(1:20)%>%
+  neat()
+# No
+ds2 %>% group_by(shield_no) %>%
+  summarize(n_duplicate_shields = n_distinct(unique_mos_id)) %>%
+  filter(n_duplicate_shields > 1 & n_duplicate_shields< 458) %>%
+  summarize(
+    n_duplicated_shields = sum(n_duplicate_shields)
+  )%>%
+  neat()
+# 402 shields have at least one duplicate
+ds2 %>% group_by(shield_no) %>%
+  summarize(n_duplicates = n_distinct(unique_mos_id)) %>%
+  filter(n_duplicates > 1 & n_duplicates< 458) %>%
+  group_by(n_duplicates) %>%
+  summarize(n_shields = n())%>%
+  neat()
 # 197 Shield numbers are registered to more than 1 officer
-/
+
 ds2 %>% group_by(unique_mos_id) %>%
   summarize(n_duplicate_ids = n_distinct(shield_no)) %>%
   filter(n_duplicate_ids > 1) %>%
-  arrange(desc(n_duplicate_ids))
+  arrange(desc(n_duplicate_ids))%>%
+  neat()
+
 # Each officer has no more than 1 shield number
 
 
 # Rank of the officer (as of July 2020)
-ds2 %>% group_by(rank_now) %>% count()
-ds2 %>% group_by(rank_now, rank_abbrev_now) %>% count()
+ds2 %>% group_by(rank_now) %>% count() %>% neat()
+ds2 %>% group_by(rank_now, rank_abbrev_now) %>% count()%>%neat()
 # Abbreviated rank offers greater detail
 
 # Rank of the officer (at the time of the indident)
-ds2 %>% group_by(rank_incident) %>% count()
-ds2 %>% group_by(rank_incident, rank_abbrev_incident) %>% count()
+ds2 %>% group_by(rank_incident) %>% count()%>% neat()
+ds2 %>% group_by(rank_incident, rank_abbrev_incident) %>% count()%>% neat()
 # Abbreviated rank offers greater detail
 
 # Ethnicity
-ds2 %>% group_by(mos_ethnicity) %>% count()
-ds2 %>% group_by(complainant_ethnicity) %>% count()
+ds2 %>% group_by(mos_ethnicity) %>% count()%>% neat()
 
 # Gender
-ds2 %>% group_by(mos_gender) %>% count()
-ds2 %>% group_by(complainant_gender) %>% count()
+ds2 %>% group_by(mos_gender) %>% count()%>% neat()
+
+# Age
+ds2 %>% TabularManifest::histogram_continuous("mos_age_incident")
 
 
+# ---- complainant-1 ------------------------------------------------------
 
+# ---- complainant-2 ------------------------------------------------------
+ds2 %>% group_by(complainant_ethnicity) %>% count()%>% neat()
+ds2 %>% group_by(complainant_gender) %>% count()%>% neat()
+ds2 %>% TabularManifest::histogram_continuous("complainant_age_incident")
 
+# However,
+ds0 %>%
+  group_by(complainant_age_incident) %>%
+  summarize(n = n()) %>%
+  arrange(complainant_age_incident)%>% neat()
+# There appears to be some data errors in complainant age
+# all records with age < 10 are transformed into NA
+ds2 %>%
+  group_by(complainant_age_incident) %>%
+  summarize(n = n()) %>%
+  arrange(complainant_age_incident)%>% neat()
+
+# ---- complainant-3 ------------------------------------------------------
+
+# ---- contact-1 ------------------------------------------------------
+
+# ---- contact-2 ------------------------------------------------------
 
 # ---- save-to-disk --------------------------------------------------------
 
 # ds1 %>% readr::write_rds("./data-unshared/derived/dto.rds")
 
 # ----- publisher --------------------
-# path <- "./analysis/0-greeter/0-greeter.Rmd"
-# rmarkdown::render(
-#   input = path ,
-#   output_format=c(
-#     "html_document"
-#     # ,"word_document"
-#   ),
-#   clean=TRUE
-# )
+path <- "./analysis/1-first-look/1-first-look.Rmd"
+rmarkdown::render(
+  input = path ,
+  output_format=c(
+    "html_document"
+    # ,"word_document"
+  ),
+  clean=TRUE
+)
 
 
