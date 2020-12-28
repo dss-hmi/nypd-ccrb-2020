7262c3f6eafb216d50829bd1458460b5c9674dd1
Andriy V. Koval <koval.andrey@gmail.com>
2020-12-28 16:35:50 -0500

    Update 2-basic-graphs.R

--- analysis/2-basic-graphs/2-basic-graphs.R
+++ analysis/2-basic-graphs/2-basic-graphs.R
@@ -33,13 +33,14 @@ metadata <- readr::read_csv("./data-public/metadata/better-labels.csv")
 # ---- inspect-data -----------------------------------------------------------
 skimr::skim(ds0)
 ds0 %>% pryr::object_size()
-
+ds0 %>% glimpse()
 # -----tweak-data-1 -------------------------------------------------------------
 
 
 # -----tweak-data -------------------------------------------------------------
-
-# ds2 - adjust the columns
+mos_rank_order <- c("Police Officer", "Detective", "Sergeant", "Lieutenant",
+                    "Captain", "Deputy Inspector", "Inspector","Chief")
+# ds1 - adjust the columns
 ds1 <- ds0 %>%
   select(
   # MOS - Member of Service
@@ -79,10 +80,36 @@ ds1 <- ds0 %>%
   # Contact ( between MOS and the complanant)
     ,contact_reason
     ,outcome_description     # Contact outcome
+  ) %>%
+  mutate(
+    rank_now_collapsed = fct_recode(
+      rank_now,
+      "Chief" = "Chiefs and other ranks"
+      ) %>%
+    fct_relevel(mos_rank_order)
+   ,rank_incident_collapsed = fct_recode(
+     rank_incident,
+     "Chief" = "Chiefs and other ranks"
+   ) %>%
+     fct_relevel(mos_rank_order)
+
   )
 
 ds1 %>% glimpse()
 #
+
+ds1 %>% group_by(rank_now) %>% count()
+ds1 %>% group_by(rank_now_collapsed) %>% count()
+
+ds1 %>% group_by(rank_incident ) %>% count()
+ds1 %>% group_by(rank_incident_collapsed ) %>% count()
+ds1 %>% group_by(rank_incident,rank_incident_collapsed ) %>% arrange(1) %>% count()
+# the order of the ranks
+
+
+
+
+
 
 # ----- variable-groups ---------------------
 metadata %>%
@@ -118,7 +145,7 @@ metadata %>%
 
 # ---- officer-2 -------------------------
 # How many distinct officers and complaints represented in the data?
-ds2 %>% summarize(
+ds1 %>% summarize(
   n_officers = n_distinct(unique_mos_id)
   ,n_shield_ns = n_distinct(shield_no)
   ,n_complaints = n_distinct(complaint_id)
@@ -127,14 +154,14 @@ ds2 %>% summarize(
 
 # how many missing shield_no?
 # Shield No is missing from 458 officers
-ds2 %>% filter(shield_no == 0L) %>%
+ds1 %>% filter(shield_no == 0L) %>%
   summarize(n_mos_with_missing_shield_no = n_distinct(unique_mos_id))%>%
   neat()
 
 
 # Are shield numbers unique?
 # No
-ds2 %>% group_by(shield_no) %>%
+ds1 %>% group_by(shield_no) %>%
   summarize(n_duplicate_shields = n_distinct(unique_mos_id)) %>%
   filter(n_duplicate_shields > 1) %>%
   arrange(desc(n_duplicate_shields)) %>%
@@ -142,7 +169,7 @@ ds2 %>% group_by(shield_no) %>%
   neat()
 
 # 402 shields have at least one duplicate
-ds2 %>% group_by(shield_no) %>%
+ds1 %>% group_by(shield_no) %>%
   summarize(n_duplicate_shields = n_distinct(unique_mos_id)) %>%
   filter(n_duplicate_shields > 1 & n_duplicate_shields< 458) %>%
   summarize(
@@ -152,7 +179,7 @@ ds2 %>% group_by(shield_no) %>%
 
 
 # 197 Shield numbers are registered to more than 1 officer
-ds2 %>% group_by(shield_no) %>%
+ds1 %>% group_by(shield_no) %>%
   summarize(n_duplicates = n_distinct(unique_mos_id)) %>%
   filter(n_duplicates > 1 & n_duplicates< 458) %>%
   group_by(n_duplicates) %>%
@@ -160,7 +187,7 @@ ds2 %>% group_by(shield_no) %>%
   neat()
 
 # Each officer has no more than 1 shield number
-ds2 %>% group_by(unique_mos_id) %>%
+ds1 %>% group_by(unique_mos_id) %>%
   summarize(n_duplicate_ids = n_distinct(shield_no)) %>%
   filter(n_duplicate_ids > 1) %>%
   arrange(desc(n_duplicate_ids))%>%
@@ -169,32 +196,32 @@ ds2 %>% group_by(unique_mos_id) %>%
 
 
 # Rank of the officer (as of July 2020)
-ds2 %>% group_by(rank_now) %>% count() %>% neat()
-ds2 %>% group_by(rank_now, rank_abbrev_now) %>% count()%>%neat()
+ds1 %>% group_by(rank_now) %>% count() %>% neat()
+ds1 %>% group_by(rank_now, rank_abbrev_now) %>% count()%>%neat()
 # Abbreviated rank offers greater detail
 
 # Rank of the officer (at the time of the indident)
-ds2 %>% group_by(rank_incident) %>% count()%>% neat()
-ds2 %>% group_by(rank_incident, rank_abbrev_incident) %>% count()%>% neat()
+ds1 %>% group_by(rank_incident) %>% count()%>% neat()
+ds1 %>% group_by(rank_incident, rank_abbrev_incident) %>% count()%>% neat()
 # Abbreviated rank offers greater detail
 
 # Ethnicity
-ds2 %>% group_by(mos_ethnicity) %>% count()%>% neat()
+ds1 %>% group_by(mos_ethnicity) %>% count()%>% neat()
 
 # Gender
-ds2 %>% group_by(mos_gender) %>% count()%>% neat()
+ds1 %>% group_by(mos_gender) %>% count()%>% neat()
 
 # Age
-ds2 %>% TabularManifest::histogram_continuous("mos_age_incident")
+ds1 %>% TabularManifest::histogram_continuous("mos_age_incident")
 
 # ---- officer-2 -------------------------
 
 # ---- complainant-1 ------------------------------------------------------
 
 # ---- complainant-2 ------------------------------------------------------
-ds2 %>% group_by(complainant_ethnicity) %>% count()%>% neat()
-ds2 %>% group_by(complainant_gender) %>% count()%>% neat()
-ds2 %>% TabularManifest::histogram_continuous("complainant_age_incident")
+ds1 %>% group_by(complainant_ethnicity) %>% count()%>% neat()
+ds1 %>% group_by(complainant_gender) %>% count()%>% neat()
+ds1 %>% TabularManifest::histogram_continuous("complainant_age_incident")
 
 # However,
 ds0 %>%
@@ -203,35 +230,35 @@ ds0 %>%
   arrange(complainant_age_incident)%>% neat()
 # There appears to be some data errors in complainant age
 # all records with age < 10 are transformed into NA
-ds2 %>%
+ds1 %>%
   group_by(complainant_age_incident) %>%
   summarize(n = n()) %>%
   arrange(complainant_age_incident)%>% neat()
 
 # ---- complainant-3 ------------------------------------------------------
-ds2 %>% glimpse()
+ds1 %>% glimpse()
 
 
 # ---- complaint-1 ------------------------------------------------------
-ds2 %>% group_by(fado_type) %>% count()%>% arrange(desc(n))
-ds2 %>%
+ds1 %>% group_by(fado_type) %>% count()%>% arrange(desc(n))
+ds1 %>%
   group_by(fado_type,allegation) %>%
   count() %>%
   arrange(fado_type,desc(n)) %>%
   print(n = nrow(.))
 # ---- complaint-2 ------------------------------------------------------
-ds2 %>% group_by(board_disposition) %>% count()%>% arrange(desc(n))
+ds1 %>% group_by(board_disposition) %>% count()%>% arrange(desc(n))
 
 # ---- contact-1 ------------------------------------------------------
-ds2 %>% group_by(contact_reason) %>% count() %>% arrange(desc(n))
+ds1 %>% group_by(contact_reason) %>% count() %>% arrange(desc(n))
 
-ds2 %>% group_by(outcome_description) %>% count() %>% arrange(desc(n))
+ds1 %>% group_by(outcome_description) %>% count() %>% arrange(desc(n))
 
 # ---- contact-2 ------------------------------------------------------
 
 # ---- save-to-disk --------------------------------------------------------
 
-ds2 %>% readr::write_rds("./data-unshared/derived/dto.rds")
+ds1 %>% readr::write_rds("./data-unshared/derived/dto.rds")
 
 # ----- publisher --------------------
 path <- "./analysis/2-basic-graphs/2-basic-graphs.Rmd"
